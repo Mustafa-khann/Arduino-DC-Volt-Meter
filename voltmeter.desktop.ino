d53fd8bcbd0e3380525ade846f7949a485381b9d
Mustafa khan <58285154+Mustafa-khann@users.noreply.github.com>
2022-01-11 11:22:48 +0500

    Update voltmeter.desktop.ino

--- voltmeter.desktop.ino
+++ voltmeter.desktop.ino
@@ -13,12 +13,11 @@ void loop(){
    val = analogRead(analogInput);//reads the analog input
    Vout = (val * 5.00) / 1024.00; // formula for calculating voltage out i.e. V+, here 5.00
    Vin = Vout / (R2/(R1+R2)); // formula for calculating voltage in i.e. GND
-   if (Vin<0.09) //condition 
+   if (Vin<0.09)//condition 
    {
-     Vin=0.00; //statement to quash undesired reading !
+     Vin=0.00;//statement to quash undesired reading !
   } 
 Serial.print("\t Voltage of the given source = ");
 Serial.print(Vin);
-Serial.println("");
 delay(1000); //for maintaining the speed of the output in serial moniter
 }
